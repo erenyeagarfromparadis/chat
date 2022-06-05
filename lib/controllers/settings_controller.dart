24795d6af6503ab9f96e244bfe1c78c74c76d641
unknown <marley321marley@gmail.com>
2022-06-05 20:57:17 +0300

    bb

--- lib/controllers/settings_controller.dart
+++ lib/controllers/settings_controller.dart
@@ -2,7 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_chat_app_learning/models/user_model.dart';
 import 'package:get/get.dart';
 import 'package:flutter_chat_app_learning/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 class SettingsController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
   Rx<UserModel> userModel = UserModel(uId:"", name:"" , image:"", number:"", status:"", typing:"", online:"").obs;
-  
+
+  @override
+  void onInit() {
+    super.onInit();
+    // FirebaseFirestore.instance.collection("users").doc(auth.currentUser?.uid).get()
+  }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -12,18 +12,39 @@ class SettingsScreen  extends GetView<SettingsController> {
   appBar: AppBar(
     elevation: 0 ,
     automaticallyImplyLeading: false,
+    backgroundColor: Theme.of(context).backgroundColor,
     flexibleSpace: SafeArea(
       child: GestureDetector(
         onTap: (){},
         child: Container(
+
           padding: EdgeInsets.only(left: 16,right: 16),
           child: Row(children: [
             Expanded(child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center
-            ,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Hero(tag: "profileName", child: Obx(()=>Text(controller.userModel.value.name == "" ? "No Name" : controller.userModel.value.name , style: const TextStyle(fontSize: 16 , fontWeight: FontWeight.w600)))),
-            ],))
+              Hero(
+                tag: "profileName",
+                child: Obx(()=>Text(
+                  controller.userModel.value.name == "" ? "No Name" : controller.userModel.value.name ,
+                  style: const TextStyle(fontSize: 16 , fontWeight: FontWeight.w600),
+                ),
+                ),
+              ),
+              SizedBox(height: 6,),
+              Text('view and edit profile' , style: TextStyle(fontSize: 14 , fontWeight: FontWeight.w400 , color: Theme.of(context).primaryColor)
+                ,),
+
+            ],),),
+            Hero(
+              tag: "profileImage",
+              child: Obx(
+                    ()=>controller.userModel.value.image == "" ? const CircleAvatar(backgroundImage:  AssetImage('assets/images/default.png') , maxRadius: 20,):
+                CircleAvatar(backgroundImage: NetworkImage(controller.userModel.value.image) , maxRadius: 20,
+                ),
+              ),
+            ),
+
           ],),
           ),
 
@@ -31,6 +52,62 @@ class SettingsScreen  extends GetView<SettingsController> {
     ),
 
   ),
+    body:Padding(
+      padding: const EdgeInsets.only(left: 16 , top: 20 , right:16),
+      child: Column(
+        children: [
+          Padding( padding: const EdgeInsets.symmetric(vertical: 8), child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: (){},
+              child: ListTile(
+                tileColor: Theme.of(context).disabledColor.withOpacity(0.1),
+                leading: Icon(Icons.language_rounded ,
+                color: Theme.of(context).primaryColor,
+                ),
+                title: const Text("change language"),
+                trailing: const Text("EN"),
+              ),
+            ),
+          ),),
+          ListTile(
+            tileColor: Theme.of(context).disabledColor.withOpacity(0.1),
+            leading: Icon(Icons.help_rounded ,
+              color: Theme.of(context).primaryColor,
+            ),
+            title: const Text("help"),
+
+          ),
+          Padding( padding: const EdgeInsets.symmetric(vertical: 8), child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: (){},
+              child: ListTile(
+                tileColor: Theme.of(context).disabledColor.withOpacity(0.1),
+                leading: Icon(Icons.info_outline_rounded ,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: const Text("About the app"),
+              ),
+            ),
+          ),),
+          Padding( padding: const EdgeInsets.symmetric(vertical: 8), child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: (){controller.userModel.},
+              child: ListTile(
+                tileColor: Theme.of(context).disabledColor.withOpacity(0.1),
+                leading: Icon(Icons.logout_rounded ,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: const Text("Logout"),
+              ),
+            ),
+          ),),
+
+        ],
+      ),
+    ),
   );
   }
 }
